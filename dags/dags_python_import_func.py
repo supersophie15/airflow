4be377e6adea40a31da50b61f02888c95f607e94
sophie <sujeong.bak@gmail.com>
2025-05-16 22:33:00 +0900

    수정

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -2,19 +2,17 @@ from airflow import DAG
 import pendulum
 import datetime
 from airflow.operators.python import PythonOperator
-from commom.commom_func import get_sftp 
-
-#python이 클래스를 가져오는 경로를 plugins를 인식하도록 설정해주어야함. 
-#airflow는 plugins 폴더까지 python path 로 잡는다.> 따라서 맨 앞에 plugins를 작성하면 안된다. 이때 코드에러가 나기 때문에 .env 파일을 하나 작성한다. 
+from common.common_func import get_sftp
 
 with DAG(
-    dat_id='dags_python_import_func',
+    dag_id="dags_python_import_func",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
-    
+
     task_get_sftp = PythonOperator(
         task_id='task_get_sftp',
         python_callable=get_sftp
     )
+
