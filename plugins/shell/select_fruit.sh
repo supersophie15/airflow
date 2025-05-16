66a774d5eeee4ffbb5f223e2ee5ec22c8fe0492b
sophie <sujeong.bak@gmail.com>
2025-05-16 17:22:07 +0900

    파일복사하여 수정

--- plugins/shell/select_fruit.sh
+++ plugins/shell/select_fruit.sh
@@ -1,10 +1,10 @@
 FRUIT=$1
-if [$FRUIT == "APPLE"]; then
-        echo "You selected Apple!"
-elif [$FRUIT == "ORANGE"]; then
-        echo "You selected ORANGE!"
-elif [$FRUIT == "GRAPE"]; then
-        echo "You selected GRAPE!"
+if [ $FRUIT == APPLE ];then
+	echo "You selected Apple!"
+elif [ $FRUIT == ORANGE ];then
+	echo "You selected Orange!"
+elif [ $FRUIT == GRAPE ];then
+	echo "You selected Grape!"
 else
-        echo "You selected other Fruit!"
+	echo "You selected other Fruit!"
 fi
