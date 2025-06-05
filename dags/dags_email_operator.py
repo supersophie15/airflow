a3a4f73a38a6a598a457ad7488b3bfb135119312
sophie <sujeong.bak@gmail.com>
2025-06-06 00:22:24 +0900

    메일주소 수정

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -15,7 +15,7 @@ with DAG(
 ) as dag:
     send_email_task = EmailOperator(
         task_id='send_email_task',
-        to='hjkim_sun@naver.com',
+        to='sujeong.bak@gmail.com',
         subject='Airflow 성공메일',
         html_content='Airflow 작업이 완료되었습니다'
     )
